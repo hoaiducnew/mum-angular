bcc903329bccfccb80908f5b1e42b76b25e8f174
DUKE-PC\duke <hoaiducnew@gmail.com>
2017-10-19 22:11:16 -0500

    reset project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,10 @@
 import { Component } from '@angular/core';
-import {Condition, Product} from "./product";
-
-const PRODUCTS: Product[] = [
-    {id:1, name:'iPhone', description:'iPhone 8', price:600, condition: Condition.New, category:'phone'},
-    {id:2, name:'iPad', description:'iPad Air 2', price:400, condition: Condition.Used, category:'tablet'}
-]
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h2>{{title}}</h2>
-    <ul>
-        <li *ngFor="let product of products">
-            <span>{{product.id}}</span> - {{product.name}}
-        </li>
-    </ul>
-  `,
-  styles: [`
-  
-  `]
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Products';
-  products = PRODUCTS;
-  selectedProduct: Product;
 
-  onSelect(product: Product): void {
-    this.selectedProduct = product;
-  }
 }
